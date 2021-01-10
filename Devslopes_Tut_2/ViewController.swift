bb65a0d55371d085cc1d63c9d140c963564dc8ce
Dominik <dominik.dolezall@gmail.com>
2021-01-10 19:02:00 +0100

    Initial Commit

---
+++ Devslopes_Tut_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Devslopes_Tut_2
+//
+//  Created by Dominik Doležal on 10/01/2021.
+//  Copyright © 2021 Do-Simeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
